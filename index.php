893f1d14371e247f8e9e69bda94c425308f284ab
Himura Kazuto <glagol15@gmail.com>
2016-04-21 23:45:49 +0300

    starting to work

--- index.php
+++ index.php
@@ -25,15 +25,12 @@
         $quote = file_get_contents("http://$_SERVER[HTTP_HOST]/quote.php?id=" . $id);
     } else {
         $out = file_get_contents("http://$_SERVER[HTTP_HOST]/randomquote.php?full");
-        
-        echo $out;
         $out = json_decode($out, true);
-        
-        //$id = $out['Id'];
-        //$quote = $out['Text'];
-        //echo "<script type=\"text/javascript\">";
-        //echo "    $(function() {rewrite_url(". $id .");});";
-        //echo "</script>";
+        $id = $out['Id'];
+        $quote = $out['Text'];
+        echo "<script type=\"text/javascript\">";
+        echo "    $(function() {rewrite_url(". $id .");});";
+        echo "</script>";
     }
         
         echo "\t\t<meta property=\"og:url\" content=\"http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]\" />\n";
