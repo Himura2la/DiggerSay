676cb73af961294455922d7bf3cbedea46b203f8
markelych <mms@markelych.ru>
2016-04-21 10:32:18 +0300

    moder-test

---
+++ moder-test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=0.7, maximum-scale=0.7">
+		<link media="screen" href="css/style.css" type="text/css" rel="stylesheet" />
+		<link href='https://fonts.googleapis.com/css?family=Ubuntu+Condensed&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/favicon.ico" />
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+        <script type="text/javascript" src="main.js"></script>
+
+        
+        <title>Скажет диггер</title>
+	</head>
+	<body>
+<?php
+require 'db.php';
+		$stmt = $mysqli->prepare("SELECT * FROM quotes_main WHERE Active=0");        
+        $stmt->execute();
+        $res = $stmt->get_result();
+        $row = $res->fetch_assoc();
+        echo json_encode($row, JSON_UNESCAPED_UNICODE);
+?>
+	</body>
+</html>
