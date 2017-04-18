eeff636f87c7c1be8e60c48bf03fe06d68b31094
Himura Kazuto <glagol15@gmail.com>
2017-04-19 00:03:09 +0300

    Text wrap and align

--- quote_pic.php
+++ quote_pic.php
@@ -1,14 +1,51 @@
 <?php
 
+/*  http://stackoverflow.com/questions/5746537/
+    Make sure to set the font on the ImagickDraw Object first!
+    @param image the Imagick Image Object
+    @param draw the ImagickDraw Object
+    @param text the text you want to wrap
+    @param maxWidth the maximum width in pixels for your wrapped "virtual" text box
+    @return an array of lines and line heights
+*/
+function wordWrapAnnotation($image, $draw, $text, $maxWidth) {   
+    $text = trim($text);
+    $words = preg_split('%\s%', $text, -1, PREG_SPLIT_NO_EMPTY);
+    $lines = array();
+    $i = 0;
+    $lineHeight = 0;
+    while (count($words) > 0) {   
+        $metrics = $image->queryFontMetrics($draw, implode(' ', array_slice($words, 0, ++$i)));
+        $lineHeight = max($metrics['textHeight'], $lineHeight);
+        
+        // check if we have found the word that exceeds the line width
+        if ($metrics['textWidth'] > $maxWidth or count($words) < $i)  {   
+            // handle case where a single word is longer than the allowed line width 
+            // (just add this as a word on its own line?)
+            if ($i == 1)
+                $i++;
+            $lines[] = implode(' ', array_slice($words, 0, --$i));
+            $words = array_slice($words, $i);
+            $i = 0;
+        }   
+    }   
+    return array($lines, $lineHeight);
+}
+
+
 $image = new Imagick("images/vk_repost.jpg");
+$width = $image->getImageWidth();
 $draw = new ImagickDraw();
 
 $draw->setFillColor('#267f4c');
+$draw->setTextAlignment(Imagick::ALIGN_CENTER);
 
 $draw->setFont('font/Ubuntu-C.ttf');
 $draw->setFontSize(45);
 
-$image->annotateImage($draw, 10, 150, 0, $_GET["t"]);
+list($lines, $lineHeight) = wordWrapAnnotation($image, $draw, $_GET["t"], $width-20);
+for($i = 0; $i < count($lines); $i++)
+    $image->annotateImage($draw, $width/2, $lineHeight + $i*$lineHeight, 0, $lines[$i]);
 
 $image->setImageFormat('png');
 
