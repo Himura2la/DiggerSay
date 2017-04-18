06a8a73bc34d56672606f2d359c6c4b200c047e3
Himura Kazuto <glagol15@gmail.com>
2017-04-18 23:15:06 +0300

    font

--- quote_pic.php
+++ quote_pic.php
@@ -3,9 +3,9 @@
 $bg = new Imagick("images/vk_repost.jpg");
 $draw = new ImagickDraw();
 
-$draw->setFillColor('black');
+$draw->setFillColor('#267f4c');
 
-$draw->setFont('Bookman-DemiItalic');
+$draw->setFont('font/Ubuntu-C.ttf');
 $draw->setFontSize( 30 );
 
 $image->annotateImage($draw, 10, 45, 0, 'nya');
