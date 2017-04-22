30eb567b01fc7fe9aae9f6ccb5a5f212430c3ab1
himura <himura@kampai-nb>
2017-04-22 19:46:17 +0300

    expand lines

--- quote_pic.php
+++ quote_pic.php
@@ -60,7 +60,7 @@ if (count($lines) > 5) {
 }
 
 for($i = 0; $i < count($lines); $i++)
-    $image->annotateImage($draw, $width/2, $lineHeight + $i*$lineHeight, 0, $lines[$i]);
+    $image->annotateImage($draw, $width/2, $lineHeight + $i*$lineHeight, 0, " " . $lines[$i] . " ");
 
 $image->setImageFormat('png');
 
