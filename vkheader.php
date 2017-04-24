227132cfb98e67f8510b1e37ae6bcca38e911144
markelych <mms@markelych.ru>
2017-04-25 00:08:25 +0300

    test

--- vkheader.php
+++ vkheader.php
@@ -1,14 +1,14 @@
 <?php
-// Нужен ТОКЕН ГРУППЫ СО ВСЕМИ ПРАВАМИ!
-$token = "b423a7253493821608f90897b901ac3c269b8bf903265a782375da7d6a367cad0580c44cdb2af548d7291";
+// РќСѓР¶РµРЅ РўРћРљР•Рќ Р“Р РЈРџРџР« РЎРћ Р’РЎР•РњР РџР РђР’РђРњР!
+$token = "0b423a7253493821608f90897b901ac3c269b8bf903265a782375da7d6a367cad0580c44cdb2af548d7291";
 $cover_path = dirname(__FILE__).'/cover/'.mt_rand(1,3).'.jpg';
 $post_data = array("photo" => "@".$cover_path);
-// рандомно выбирается фотография 1,2 или 3
+// СЂР°РЅРґРѕРјРЅРѕ РІС‹Р±РёСЂР°РµС‚СЃСЏ С„РѕС‚РѕРіСЂР°С„РёСЏ 1,2 РёР»Рё 3
 $upload_url = file_get_contents("https://api.vk.com/method/photos.getOwnerCoverPhotoUploadServer?group_id=119527646&crop_x2=1590&access_token=".$token);
 $url = json_decode($upload_url)->response->upload_url;
 echo $url;
-// урл для загрузки фото получен
-// фото отправлено
+// СѓСЂР» РґР»СЏ Р·Р°РіСЂСѓР·РєРё С„РѕС‚Рѕ РїРѕР»СѓС‡РµРЅ
+// С„РѕС‚Рѕ РѕС‚РїСЂР°РІР»РµРЅРѕ
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, true);
@@ -19,9 +19,9 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
 $result = json_decode(curl_exec($ch),true);
 echo '<pre>';
 print_r($result);
-echo '<pre>сохраняем фото';
+echo '<pre>СЃРѕС…СЂР°РЅСЏРµРј С„РѕС‚Рѕ';
 $safe = file_get_contents("https://api.vk.com/method/photos.saveOwnerCoverPhoto?hash=".$result['hash']."&photo=".$result['photo']."&access_token=".$token);
 echo '<pre>';
 print_r($safe);
-// все готово
+// РІСЃРµ РіРѕС‚РѕРІРѕ
 ?>
