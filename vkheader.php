98e7ee99354c43ad1a8d58e13685d3c31a65baf3
markelych <mms@markelych.ru>
2017-04-25 00:29:13 +0300

    debug

--- vkheader.php
+++ vkheader.php
@@ -1,7 +1,7 @@
 <?php
 // Нужен ТОКЕН ГРУППЫ СО ВСЕМИ ПРАВАМИ!
 $token = "b423a7253493821608f90897b901ac3c269b8bf903265a782375da7d6a367cad0580c44cdb2af548d7291";
-$cover_path = dirname(__FILE__).'/cover/'.mt_rand(1,3).'.jpg';
+$cover_path = dirname(__FILE__).'\\cover\\'.mt_rand(1,3).'.jpg';
 //
 echo '<pre>';
 echo $cover_path;
@@ -10,6 +10,7 @@ $post_data = array('photo' => $_SERVER['DOCUMENT_ROOT'].$cover_path);
 // рандомно выбирается фотография 1,2 или 3
 $upload_url = file_get_contents("https://api.vk.com/method/photos.getOwnerCoverPhotoUploadServer?group_id=119527646&crop_x2=1590&access_token=".$token);
 $url = json_decode($upload_url)->response->upload_url;
+echo '<pre>';
 echo $url;
 // урл для загрузки фото получен
 // фото отправлено
