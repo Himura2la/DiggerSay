b620a1636163763dacc09e5ead2abd9c21f8bfba
markelych <mms@markelych.ru>
2017-04-25 00:33:02 +0300

    test

--- vkheader.php
+++ vkheader.php
@@ -5,7 +5,7 @@ $cover_path = dirname(__FILE__).'\\cover\\'.mt_rand(1,3).'.jpg';
 //
 echo '<pre>';
 echo $cover_path;
-$post_data = array('photo' => $_SERVER['DOCUMENT_ROOT'].$cover_path);
+$post_data = array("photo" => $_SERVER['DOCUMENT_ROOT'].$cover_path);
 //$post_data = array('photo' => new CURLFile($cover_path, 'image/jpeg', 'image0'));
 // рандомно выбирается фотография 1,2 или 3
 $upload_url = file_get_contents("https://api.vk.com/method/photos.getOwnerCoverPhotoUploadServer?group_id=119527646&crop_x2=1590&access_token=".$token);
@@ -22,7 +22,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
 $result = json_decode(curl_exec($ch),true);
-echo '<pre>';
+echo '<pre>Отправка фото:<pre>';
 print_r($result);
 echo '<pre>сохраняем фото';
 $safe = file_get_contents("https://api.vk.com/method/photos.saveOwnerCoverPhoto?hash=".$result['hash']."&photo=".$result['photo']."&access_token=".$token);
